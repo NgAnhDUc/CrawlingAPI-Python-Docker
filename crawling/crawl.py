d0d048ce14142210c62fa939a1b0226c0f0f57b5
NgAnhDUc <86831818+NgAnhDUc@users.noreply.github.com>
2024-05-03 16:35:12 +0700

    crawling

---
+++ crawling/crawl.py
@@ -0,0 +1,21 @@
+import requests # type: ignore
+
+products = []
+url = "https://gateway.chotot.com/v1/public/ad-listing?limit=10&protection_entitlement=true&cg=5010&st=s,k&key_param_included=true"
+
+respone = requests.get(url)
+
+if respone.status_code == 200:
+    print("get success")
+    for item in respone.json().get('ads'):
+        products.append({'id': item.get('ad_id')})
+        products.append({'date': item.get('date')})
+        products.append({'body': item.get('body')})
+        products.append({'category': item.get('category')})
+        products.append({'category_name': item.get('category_name')})
+        products.append({'price': item.get('price')})
+        products.append({'subject': item.get('subject')})
+
+print(products)
+    
+    
--- mysql/init_db/init.sql
+++ mysql/init_db/init.sql
@@ -1,8 +1,19 @@
-CREATE DATABASE local_db;
-USE local_db
-CREATE TABLE books (
-	id int(10) NOT NULL AUTO_INCREMENT,
-	title varchar(30) NOT NULL DEFAULT '',
-	price int(10) NOT NULL,
-	PRIMARY KEY (id)
+CREATE DATABASE chotot_db;
+USE chotot_db,
+CREATE TABLE products (
+	ad_id INT,
+	timedate NVARCHAR(256),
+	account_id INT,
+	account_name NVARCHAR(256),
+	title NVARCHAR(256),
+	body NVARCHAR(256),
+	category INT,
+	category_name NVARCHAR(256),
+	area INT,
+	area_name NVARCHAR(256),
+	region INT,
+	region_name NVARCHAR(256),
+	price INT,
+	price_string NVARCHAR(256),
+	webp_image NVARCHAR(256)
 );
